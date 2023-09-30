b4d31a4eb17d7f7c752d8472c13fec727dff83b7
Vitalii Tomenko <vitalikxxx985@gmail.com>
2023-09-30 16:58:10 +0200

    delet styled

--- src/styled.js
+++ src/styled.js
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from "styled-components";
 import background from "./background.jpg";
 
 export const GlobalStyles = createGlobalStyle`
