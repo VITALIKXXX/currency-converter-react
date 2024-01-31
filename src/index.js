ffa35a2f6c6fea73361610671bb714ea9a05b17b
Vitalii Tomenko <vitalikxxx985@gmail.com>
2024-01-31 06:06:39 +0100

    delete comit

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { GlobalStyles } from './styled';
 
-//test
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
